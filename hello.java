5e45032f2954b69d29f928bd592b7a3ee9b88505
prashanthrajkade <prashanthrajkade@gmail.com>
2026-01-11 22:54:35 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Java");
                 System.out.println("Hello from  webhook Jenkins Java");
+                        System.out.println("Hello from  webhook Jenkins Java");
+
 
     }
 }
